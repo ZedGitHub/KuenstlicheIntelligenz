93392f7d6c925e931b526877f05d6741f647d32b
robertzoeke <s3rozoek@stud.hszg.de>
2014-10-07 13:19:54 +0200

    initial commit

---
+++ machineLearning/de/hszg/learner/DummyDataCreator.java
@@ -0,0 +1,36 @@
+package de.hszg.learner;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DummyDataCreator {
+
+	private static final String filename = "DummyData.dat";
+	DummyDataCreator(){
+		FeatureVector[] f = new FeatureVector[6];
+		f[0] = new DummyFeatureVector(4,2,1,Concept.Stop);
+		f[1] = new DummyFeatureVector(1,2,3,Concept.Stop);
+		f[2] = new DummyFeatureVector(4,5,6,Concept.Vorfahrt);
+		f[3] = new DummyFeatureVector(1,5,3,Concept.RechtsAbbiegen);
+		f[4] = new DummyFeatureVector(3,2,5,Concept.Stop);
+		f[5] = new DummyFeatureVector(5,2,1,Concept.LinksAbbiegen);
+		
+		List<FeatureVector> res = new LinkedList<>();
+		for(FeatureVector fv : f) res.add(fv);
+		try{
+			ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(filename)));
+			out.writeObject(res);
+			out.close();
+		}catch(Throwable t){
+			System.out.println("DummyDataCreator: Could not create DummyData.dat");
+			t.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		new DummyDataCreator();
+	}
+
+}
---
+++ machineLearning/de/hszg/learner/FeatureVector.java
@@ -0,0 +1,24 @@
+package de.hszg.learner;
+
+import java.io.Serializable;
+
+public interface FeatureVector extends Serializable{
+	
+	/** 
+	 * @return the concept this feature vector represents
+	 */
+	Concept getConcept();
+	
+	/** 
+	 * @return the number of features in the feature vector
+	 */
+	int getNumFeatures();
+	
+	/**
+	 * 
+	 * @param i the index of the feature in the vector
+	 * @return the value of the feater with index i
+	 */
+	int getFeatureValue(int i);
+
+}
