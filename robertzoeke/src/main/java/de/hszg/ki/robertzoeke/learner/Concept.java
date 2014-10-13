6e49f329e21dbed45d4db6c61e9bbdd4be48e095
robertzoeke <s3rozoek@stud.hszg.de>
2014-10-13 10:02:59 +0200

    added dummy classes

---
+++ robertzoeke/src/main/java/de/hszg/ki/robertzoeke/learner/Concept.java
@@ -0,0 +1,5 @@
+package de.hszg.ki.robertzoeke.learner;
+
+public enum Concept {
+	Unknown, Vorfahrt, Stop, RechtsAbbiegen, LinksAbbiegen,  //TODO: die Zeichen aus den Dateien
+}
---
+++ robertzoeke/src/main/java/de/hszg/ki/robertzoeke/learner/DummyLearner.java
@@ -0,0 +1,27 @@
+package de.hszg.ki.robertzoeke.learner;
+
+import java.util.List;
+
+
+/** a dummy class to implement a learner that will not do any 
+ * learning and will the think, everything corresponds to 
+ * the same concept. 
+ * 
+ * @author gr
+ *
+ */
+public class DummyLearner implements Learner {
+
+	@Override
+	public void learn(List<FeatureVector> trainingSet) {
+		// wir machen gar nichts und bleiben dumm
+
+	}
+
+	@Override
+	public Concept classify(FeatureVector example) {
+		// weil wir nichts gelernt haben glauben wir, alles sind Stopschilder
+		return Concept.Stop;
+	}
+
+}
