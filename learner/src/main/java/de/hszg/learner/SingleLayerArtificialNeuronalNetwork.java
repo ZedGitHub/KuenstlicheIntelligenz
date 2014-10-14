bef0e5795273dc022542df25f0779403a4419a72
danielmpla <danielmpla@gmail.com>
2014-10-14 13:40:43 +0200

    Created SingleLayerArtificialNeuronalNetwork.java

---
+++ learner/src/main/java/de/hszg/learner/SingleLayerArtificialNeuronalNetwork.java
@@ -0,0 +1,18 @@
+package de.hszg.learner;
+
+import java.util.List;
+
+/**
+ * Created by Daniel on 14.10.2014.
+ */
+public class SingleLayerArtificialNeuronalNetwork implements Learner {
+    @Override
+    public void learn(List<FeatureVector> trainingSet) {
+
+    }
+
+    @Override
+    public Concept classify(FeatureVector example) {
+        return null;
+    }
+}
