d200eab9e779a1b47fd44a98e02988b05c458b4d
on-delete <akr.tester@googlemail.com>
2014-10-21 14:48:35 +0200

    Implemented the learn function for the AehnlichkeitsLearner

--- learner/src/main/java/de/hszg/learner/AehnlichkeitsLearner.java
+++ learner/src/main/java/de/hszg/learner/AehnlichkeitsLearner.java
@@ -1,6 +1,8 @@
 package de.hszg.learner;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import de.hszg.learner.featureVector.FeatureVector;
 
@@ -12,10 +14,15 @@ import de.hszg.learner.featureVector.FeatureVector;
 
 public class AehnlichkeitsLearner implements Learner{
 
+	private List<FeatureVector> learnedVectorList = new ArrayList<FeatureVector>();
+	
 	@Override
 	public void learn(List<FeatureVector> trainingSet) {
-		// TODO Auto-generated method stub
 		
+		for(int i = 0; i<Concept.values().length; i++){
+			learnedVectorList.add(calculateLearnedVector(splitFeatureVector(trainingSet, Concept.values()[i]), Concept.values()[i]));
+		}
+	
 	}
 
 	@Override
@@ -24,4 +31,70 @@ public class AehnlichkeitsLearner implements Learner{
 		return null;
 	}
 	
+	private FeatureVector calculateLearnedVector(List<FeatureVector> splitFeatureVector, Concept concept) {
+
+		int featureSum = 0;
+		List<Integer> featureList = new ArrayList<Integer>();
+		
+		for(int i = 0; i < splitFeatureVector.get(0).getNumFeatures(); i++){
+
+			featureSum = 0;
+			
+			for(int j =0; j<splitFeatureVector.size(); j++){
+				featureSum+=splitFeatureVector.get(j).getFeatureValue(i);
+			}
+			
+			if(featureSum == 0){
+				featureList.add(0);
+			}
+			else{
+				featureList.add(featureSum/splitFeatureVector.size());
+			}
+		}
+		
+		FeatureVector tempVector = new LearnedFeatureVector(featureList, concept);
+		
+		return tempVector;
+	}
+
+	private List<FeatureVector> splitFeatureVector(List<FeatureVector >featureVectorList, Concept concept){
+		
+		List<FeatureVector> temp = featureVectorList.stream().filter(f -> f.getConcept().equals(concept)).collect(Collectors.toList());
+		
+		return temp;
+	}
+	
+	public class LearnedFeatureVector implements FeatureVector {
+
+		private Concept concept;
+		private int[] feature;
+		
+		public LearnedFeatureVector(List<Integer> featureList, Concept conceptInput){
+			feature = new int[featureList.size()];
+			
+			for(int i = 0; i<featureList.size(); i++){
+				feature[i] = featureList.get(i);
+			}
+			
+			concept = conceptInput;
+		}
+		
+		@Override
+		public Concept getConcept() {
+			return concept;
+		}
+
+		@Override
+		public int getNumFeatures() {
+			return feature.length;
+		}
+
+		@Override
+		public int getFeatureValue(int i) {
+			return feature[i];
+		}
+		
+	}
 }
+
+
